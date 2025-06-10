cb83da356559fd95260149f82c7ee1d32a4a987e
Nira Elangovan <admin@nbalaji.altostrat.com>
2025-06-10 15:50:46 +0000

    e

--- includes/constants.js
+++ includes/constants.js
@@ -1,10 +1,15 @@
 function baseSchema(domain) {
     var output_table = dataform.projectConfig.vars.weather_export_dataset + "v1";
-    return output_table + "_" + domain + "_base" ;
+    return output_table + "_" + domain + "_base";
 }
 
 const RAW_DATASET_NAME = "raw_data";
 const STAGING_DATASET_NAME = "staging_data";
 const FACT_TABLE_PREFIX = "fact_";
 
-module.exports = {baseSchema,  RAW_DATASET_NAME, STAGING_DATASET_NAME, FACT_TABLE_PREFIX,};
+module.exports = {
+    baseSchema,
+    RAW_DATASET_NAME,
+    STAGING_DATASET_NAME,
+    FACT_TABLE_PREFIX,
+};
